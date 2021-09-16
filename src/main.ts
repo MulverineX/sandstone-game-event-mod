134380d73ad52635a8cd2f019434185ad5c4ce5b
Mulverine <mulverin3@gmail.com>
2021-09-15 21:47:20 -0600

    Update Sandstone, add custom data util, remove label

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import * as path from 'path';
-import { env } from 'process';
-import { CustomResource, LiteralUnion, MCFunctionInstance, PredicateCondition, PredicateInstance, SelectorClass, rel, Coordinates, Selector } from 'sandstone';
-import { CustomResourceFactory, CustomResourceInstance } from 'sandstone/resources/Custom';
+import { LiteralUnion, MCFunctionInstance, PredicateCondition, PredicateInstance, SelectorClass, rel, Coordinates, Selector } from 'sandstone';
+import { CustomDataResource, CustomDataResourceInstance } from './utils';
 
 /**
  * GameEvent ID https://minecraft.gamepedia.com/Sculk_Sensor#Vibration_frequencies 
@@ -29,33 +27,7 @@ type RunFunction = string | MCFunctionInstance | ((pos: PositionTarget, entity: 
 /**
  * Exporter of GameEvent JSON files
  */
-/*export const GameEventResource = CustomResource('GameEvent', {
-   dataType: 'json',
-   extension: 'json',
-   save: ({ packName, saveLocation, namespace }) => path.join(saveLocation, packName + '/data', namespace, 'game_events')
-});*/
-
-export const GameEventNamespaces: Record<string, CustomResourceFactory<string, 'json'>> = {};
-
-export function GameEventNamespace(name: string) {
-   let namespace = env.NAMESPACE;
-   let _name = name;
-
-   if (name.indexOf(':') !== -1) {
-      const split = name.split(':');
-      namespace = split[0];
-      _name = split[1];
-   }
-
-   if (!GameEventNamespaces[namespace]) {
-      GameEventNamespaces[namespace] = CustomResource(`GameEvent.${namespace}`, {
-         dataType: 'json',
-         extension: 'json',
-         save: ({ packName, saveLocation }) => path.join(saveLocation, packName + '/data', namespace, 'game_events')
-      });
-   }
-   return [ GameEventNamespaces[namespace], _name];
-}
+export const GameEventResource = CustomDataResource('GameEvent', 'json', 'json', 'game_events');
 
 /**
  * Object to be exported to JSON for Event.
@@ -72,7 +44,7 @@ export type GameEventJSON = {
 export class GameEventInstance {
    name: string
    _gameEventJSON: GameEventJSON
-   _resourceInstance: CustomResourceInstance<string, 'json'>
+   _resourceInstance: CustomDataResourceInstance
 
    constructor (name: string, event: GameEventID, conditions: false | PredicateCondition[], run: string) {
       const data: GameEventJSON = {
@@ -85,8 +57,7 @@ export class GameEventInstance {
       this.name = name;
       this._gameEventJSON = data;
 
-      const factory = GameEventNamespace(name);
-      this._resourceInstance = (factory[0] as CustomResourceFactory<string, 'json'>).create(factory[1] as string, data);
+      this._resourceInstance = GameEventResource.create(name, data);
    }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,19 @@
-import { Label } from "./label";
-
-const namespace = { full: 'player', short: 'plyr' };
+import * as path from 'path';
+import { CustomResource, CustomResourceDataTypes } from "sandstone";
+import { CustomResourceInstance } from 'sandstone/resources/Custom';
 
 /**
- * Creates a new label
- * @param label Label/tag name
+ * Create a custom datapack resource
+ * @param type The Custom Resource type name
+ * @param dataType The type of data the file contains
+ * @param extension The extension of the file
+ * @param typePath The relative path in the datapack namespace of this resource type
  */
-export function newLabel (label: string) {
-  return Label(`${namespace.full}.${label}`)('@s');
+export function CustomDataResource(type: string, dataType: CustomResourceDataTypes, extension: string, typePath: string) {
+  return CustomResource(type, {
+    dataType, extension,
+    save: ({ packName, saveLocation, namespace }) => path.join(saveLocation, packName, 'data', namespace, typePath)
+ });
 }
+
+export type CustomDataResourceInstance = CustomResourceInstance<string, CustomResourceDataTypes>
