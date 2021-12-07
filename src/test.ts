4b9237343d8d19befa20bd08a60a26d2dea9b0d5
Mulverine <mulverin3@gmail.com>
2021-12-06 21:56:43 -0700

    Its alive!!!

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,13 @@
 import { execute, MCFunction, summon } from "sandstone";
 import GameEvent from "./main";
 
-GameEvent('foo', 'minecraft:block_destroy', {
-      condition: 'minecraft:entity_properties',
-      entity: 'this',
-      predicate: {
-         type: 'minecraft:player'
-      }
-   }, (pos, entity) => MCFunction('destroy', () => {
-      summon('minecraft:pig', pos);
-      execute.as(entity).run.say('hello');
+GameEvent('testing', 'minecraft:block_place', {
+   condition: 'minecraft:entity_properties',
+   entity: 'this',
+   predicate: {
+      nbt: "{Tags:['test']}"
    }
-))
+}, (pos, entity) => MCFunction('testing', () => {
+   summon('minecraft:pig', pos);
+   execute.as(entity).run.say('hello');
+}))
